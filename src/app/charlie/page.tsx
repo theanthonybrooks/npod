00076e555a55d109e026ed95033233d390d41c8d
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-08-21 09:42:39 +0200

    hehe: adding meows

---
+++ src/app/charlie/page.tsx
@@ -0,0 +1,9 @@
+const CharliePage = () => {
+  return (
+    <div className='flex items-center justify-center h-screen w-full text-9xl '>
+      <h1>CharliePage</h1>
+    </div>
+  )
+}
+
+export default CharliePage
