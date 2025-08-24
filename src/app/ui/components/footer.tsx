b712878134684d6a6cfeabbeb8ef0c0d4adfe093
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-08-24 17:25:34 +0200

    wip: footer padding and text styling

--- src/app/ui/components/footer.tsx
+++ src/app/ui/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <footer className="mx-auto flex w-full max-w-[85vw] flex-col items-center gap-4 py-4">
-      <div className="text-foreground mx-auto flex w-full flex-col-reverse items-center justify-around gap-1 gap-x-8 rounded-full bg-white/90 p-3 text-sm sm:max-w-max sm:flex-row sm:p-4 sm:px-8">
+      <div className="text-foreground mx-auto flex w-full max-w-max items-center justify-center gap-1 gap-x-6 rounded-full bg-white/90 p-3 px-4 text-xs text-nowrap sm:flex-row sm:gap-x-12 sm:px-10 sm:py-2.5 sm:text-sm">
         <p className={cn("!font-ubuntu")}>
           Made by{" "}
           <Link href="https://theanthonybrooks.com">Anthony Brooks</Link>
@@ -14,7 +14,7 @@ export const Footer = () => {
           <Link href="https://design.zerenoruc.com">Zeren Oruc</Link>
         </p>
       </div>
-      <div className="flex items-center gap-8 text-white/90">
+      <div className="flex items-center gap-8 text-xs text-white/90 sm:text-sm">
         <p className={cn("!font-ubuntu")}>
           <Link href="https://zerenoruc.com/impressum">Impressum</Link>
         </p>
